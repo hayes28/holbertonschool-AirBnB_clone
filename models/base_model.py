c8dae91df6e24ce8ae8dd688fc87fffa880d91af
Chris Stamper <cstampok@gmail.com>
2023-02-02 05:41:07 -0600

    push base

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Base Model Module"""
+import uuid
+import datetime
+
+
+class BaseModel:
+    """Base Model Class"""
+    def __init__(self):
+        "SPIN UP"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """STR METH"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """UPDATE TIMESTAMP"""
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """NEW RICHARD"""
+        new_richard = self.__dict__.copy()
+        new_richard["__class__"] = self.__class__.__name__
+        new_richard["created at"] = self.created_at.isoformat()
+        new_richard["updated at"] = self.updated_at.isoformat()
+        return new_richard
---
+++ tests/test_models/test_basemodel.py
@@ -0,0 +1,42 @@
+import unittest
+import uuid
+from datetime import datetime
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    def test_bas_mod_id(self):
+        bm1 = BaseModel()
+        self.assertIsInstance(bm1.id, str)
+
+    def test_bas_mod_crt(self):
+        bm1 = BaseModel()
+        self.assertIsInstance(bm1.created_at, datetime)
+
+    def test_bas_mod_upd(self):
+        bm1 = BaseModel()
+        self.assertIsInstance(bm1.updated_at, datetime)
+
+    def test_uwu_id(self):
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertNotEqual(bm1.id, bm2.id)
+
+    def test_ini_tim(self):
+        bm1 = BaseModel()
+        self.assertEqual(len(str(bm1.created_at)), len(str(bm1.updated_at)))
+
+    def test_sav_upd_met(self):
+        bm1 = BaseModel()
+        cat = bm1.created_at
+        uat = bm1.updated_at
+        bm1.save()
+        self.assertEqual(bm1.created_at, cat)
+        self.assertNotEqual(bm1.updated_at, uat)
+
+    def test_richard(self):
+        bm1 = BaseModel()
+        richard = bm1.to_dict()
+        self.assertIsInstance(richard, dict)
+
+if __name__ == "__main__":
+    unittest.main()
