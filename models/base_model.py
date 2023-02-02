146882dd13cb0f29a953108e7ee677147ec0eab4
Chris Stamper <cstampok@gmail.com>
2023-02-02 06:33:01 -0600

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -13,8 +13,8 @@ class BaseModel:
 
     def __str__(self):
         """STR METH"""
-        return "[{}] ({}) "
-        "{}".format(self.__class__.__name__, self.id, self.__dict__)
+        bname = self.__class__.__name__
+        return "[{}] ({}) {}".format(bname, self.id, self.__dict__)
 
     def save(self):
         """UPDATE TIMESTAMP"""
