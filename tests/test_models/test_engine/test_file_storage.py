048e769cf22aabec805c116fc26f381154e4cad3
Chris Stamper <cstampok@gmail.com>
2023-02-06 19:19:39 -0600

    test unit tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,3 +8,34 @@ from models.base_model import BaseModel
 
 class TestFileStorage(unittest.TestCase):
     """file storage uwunit tests"""
+    def setUp(self):
+        self.file_storage = FileStorage()
+        self.file_path = FileStorage._FileStorage__file_path
+
+    def test_all(self):
+        obj = BaseModel()
+        self.file_storage.new(obj)
+        objects = self.file_storage.all()
+        self.assertIsInstance(objects, dict)
+        self.assertEqual(obj, objects["BaseModel.{}".format(obj.id)])
+
+    def test_new(self):
+        obj = BaseModel()
+        self.file_storage.new(obj)
+        objects = self.file_storage.all()
+        self.assertEqual(obj, objects["BaseModel.{}".format(obj.id)])
+
+    def test_save(self):
+        obj = BaseModel()
+        self.file_storage.new(obj)
+        self.file_storage.save()
+        with open(self.file_path, "r") as file:
+            file_content = json.load(file)
+        self.assertEqual(obj.to_dict(), file_content["BaseModel.{}".format(obj.id)])
+
+    def tearDown(self):
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)
+
+if __name__ == "__main__":
+    unittest.main()
